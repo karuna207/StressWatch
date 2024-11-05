8937eea9bb09b3b52deeaaaeac96032165240d1a
karuna207 <karunanidhiayyamperumal@gmail.com>
2024-11-05 16:20:27 +0530

    frontend routes established

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+
+import React from 'react' 
+import { Routes,Route } from 'react-router-dom' 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; 
+import Home from './pages/Home'; 
+import Login from './pages/Login';
+import Contact from './pages/Contact'; 
+import About from './pages/About'; 
+import Navbar from './components/Navbar'; 
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]"> 
+      <ToastContainer></ToastContainer>  
+      <Navbar/> 
+      <Routes>
+        <Route path='/' element={<Home/>}></Route> 
+        <Route path="/login" element={<Login/>}></Route> 
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/about' element={<About/>}></Route> 
+
+    
+      </Routes> 
+      <Footer/>
+      
+    </div> 
+  )
 };
 
 export default App;
